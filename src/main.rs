d5b543b31de0ae75b0ff34b92907b14f9d352416
David Laban <alsuren@gmail.com>
2020-02-23 14:22:03 +0000

    sketch out command-line interface

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,58 @@ enum CommandLine {
 /// The following subcommands are forwarded to ngrok:
 /// authtoken, credits, http, start, tcp, tls, update, version, help.
 enum Ngrok {
+    /// run your project with tracing (TODO)
+    ///
+    /// Starts:
+    /// 1) your test runner (`cargo watch -x test`),
+    /// 2) your web-server (reloading whenever the tests pass)
+    /// 3) ngrok
+    /// 4) your web browser, pointing at your ngrok web root.
+    Develop,
+
+    /// list requests that returned 404 errors (TODO)
+    ///
+    /// These requests are typically ones that you haven't written handlers for
+    /// yet. They can be used for `makeview`.
+    List404s,
+
+    /// make a new route handler from the latest 404 error (TODO)
+    ///
+    /// Makes a skeleton route handler, plus an integration test that includes
+    /// the request's payload. The integration test should initially pass (if
+    /// it doesn't, please file a bug). You can then use your standard
+    /// test-driven development workflow to make the request handler do what
+    /// you want.
+    MakeView,
+
+    /// list requests that returned 500 errors (TODO)
+    ///
+    /// These requests are typically ones that caused your code to error out.
+    /// They can be used for `maketest`.
+    List500s,
+
+    /// make a regression test from the latest 500 error (TODO)
+    ///
+    /// Makes an integration test that includes the request's payload.
+    /// The integration test should initially fail. You can then use your
+    /// standard test-driven development workflow to fix the request handler.
+    MakeTest,
     #[structopt(external_subcommand)]
     Other(Vec<String>),
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let options = CommandLine::from_args();
+    let CommandLine::Ngrok(options) = CommandLine::from_args();
 
     match options {
-        CommandLine::Ngrok(Ngrok::Other(args)) => {
+        Ngrok::Other(args) => {
             run_ngrok(args)?;
         }
+        Ngrok::Develop => todo!(),
+        Ngrok::List404s => todo!(),
+        Ngrok::MakeView => todo!(),
+        Ngrok::List500s => todo!(),
+        Ngrok::MakeTest => todo!(),
     }
     Ok(())
 }
