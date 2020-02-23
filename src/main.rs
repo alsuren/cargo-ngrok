2043497cc85dd1d028492446863e226fa0a5b8a7
David Laban <alsuren@gmail.com>
2020-02-23 13:06:10 +0000

    forward ngrok subcommands to ngrok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(bin_name = "cargo")]
+enum CommandLine {
+    Ngrok(Ngrok),
+}
+
+#[derive(StructOpt)]
+/// ngrok-related helpers, for trace-driven development.
+///
+/// The following subcommands are forwarded to ngrok:
+/// authtoken, credits, http, start, tcp, tls, update, version, help.
+enum Ngrok {
+    #[structopt(external_subcommand)]
+    Other(Vec<String>),
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let options = CommandLine::from_args();
+
+    match options {
+        CommandLine::Ngrok(Ngrok::Other(args)) => {
+            assert!([
+                "authtoken",
+                "credits",
+                "http",
+                "start",
+                "tcp",
+                "tls",
+                "update",
+                "version",
+                "help",
+            ]
+            .contains(&args[0].as_str()));
+            let result = Command::new("ngrok").args(&args).status()?;
+            println!("Done {}", result);
+        }
+    }
+    Ok(())
 }
