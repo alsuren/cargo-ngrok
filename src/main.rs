87e29c5d583c439bb099c59dd9a86428e999238a
David Laban <alsuren@gmail.com>
2020-02-23 16:07:57 +0000

    rename skeleton subcommands; give credit to darklang

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,20 @@ enum CommandLine {
 }
 
 #[derive(StructOpt)]
-/// ngrok-related helpers, for trace-driven development.
+/// Use ngrok traces to drive your development process.
 ///
-/// The following subcommands are forwarded to ngrok:
-/// authtoken, credits, http, start, tcp, tls, update, version, help.
+/// This is heavily inspired by Dark's concept of "Trace Driven Development",
+/// as described at https://darklang.github.io/docs/trace-driven-development.
+#[structopt(after_help = "The following subcommands are forwarded to ngrok:
+    authtoken
+    credits
+    http
+    start
+    tcp
+    tls
+    update
+    version
+    help")]
 enum Ngrok {
     /// run your project with tracing (TODO)
     ///
@@ -24,11 +34,17 @@ enum Ngrok {
     /// 4) your web browser, pointing at your ngrok web root.
     Develop,
 
-    /// list requests that returned 404 errors (TODO)
+    /// list requests for turning into new handlers (TODO)
     ///
-    /// These requests are typically ones that you haven't written handlers for
-    /// yet. They can be used for `makeview`.
-    List404s,
+    /// Requests that receive 404 responses are typically ones that you haven't
+    /// written handlers for yet. Use `new-handler` to define routes for these.
+    _404s,
+
+    /// list requests for turning into regression tests (TODO)
+    ///
+    /// Requests that receive 500 responses are typically ones that caused your
+    /// code to error out. Use `new-test` to make regression tests for these.
+    _500s,
 
     /// make a new route handler from the latest 404 error (TODO)
     ///
@@ -37,20 +53,31 @@ enum Ngrok {
     /// it doesn't, please file a bug). You can then use your standard
     /// test-driven development workflow to make the request handler do what
     /// you want.
-    MakeView,
-
-    /// list requests that returned 500 errors (TODO)
-    ///
-    /// These requests are typically ones that caused your code to error out.
-    /// They can be used for `maketest`.
-    List500s,
+    NewHandler,
 
     /// make a regression test from the latest 500 error (TODO)
     ///
     /// Makes an integration test that includes the request's payload.
     /// The integration test should initially fail. You can then use your
     /// standard test-driven development workflow to fix the request handler.
-    MakeTest,
+    NewTest,
+
+    /// replay the latest 404 error
+    ///
+    /// Use this to smoke-test the endpoint that you just wrote with
+    /// `new-handler`.
+    #[allow(non_camel_case_types)]
+    Replay_404,
+
+    /// replay the latest 500 error
+    ///
+    /// Use this to smoke-test the endpoint that you just wrote a regression
+    /// test for, with `new-test`. We've all written our share of tests that
+    /// are completely disconnected from reality. You are forgiven.
+    #[allow(non_camel_case_types)]
+    Replay_500,
+
+    /// The rest are passed to ngrok, for convenience.
     #[structopt(external_subcommand)]
     Other(Vec<String>),
 }
@@ -63,10 +90,12 @@ fn main() -> Result<(), std::io::Error> {
             run_ngrok(args)?;
         }
         Ngrok::Develop => todo!(),
-        Ngrok::List404s => todo!(),
-        Ngrok::MakeView => todo!(),
-        Ngrok::List500s => todo!(),
-        Ngrok::MakeTest => todo!(),
+        Ngrok::_404s => todo!(),
+        Ngrok::_500s => todo!(),
+        Ngrok::NewHandler => todo!(),
+        Ngrok::NewTest => todo!(),
+        Ngrok::Replay_404 => todo!(),
+        Ngrok::Replay_500 => todo!(),
     }
     Ok(())
 }
